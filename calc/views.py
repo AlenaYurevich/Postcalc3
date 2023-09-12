1767b3f5562d371ca683ec4f2f9dd1ee7ce728b8
Alena Yurevich <110560774+AlenaYurevich@users.noreply.github.com>
2023-09-12 19:38:40 +0300

    Add files via upload

--- calc/views.py
+++ calc/views.py
@@ -1,36 +1,35 @@
-import os
 from django.shortcuts import render
-from openpyxl import load_workbook
+from letter import letter
 
 
 def hello_world(request):
-    return render(request, "index.html", {'greeting':'Hello, world!'})
+    return render(request, "index.html", {'greeting': 'Hello, world!'})
 
 
-def read_movies_from_exel(filepath):
-    movies = []
-
-    workbook = load_workbook(filename=filepath)
-    sheet = workbook.active
-
-    for row in sheet.iter_rows(min_row=2, values_only=True):
-        title, year, director, genre = row
-        movie = {
-            'title': title,
-            'year': int(year),
-            'director': director,
-            'genre': genre
-        }
-        movies.append(movie)
-
-    return movies
-
-
-def movies_view(request):
-    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files/movies.xlsx')
-    movies = read_movies_from_exel(file_path)
-    return render(request, 'index.html', {'movies': movies})
-
-
+# def read_movies_from_exel(filepath):
+#     movies = []
+#
+#     workbook = load_workbook(filename=filepath)
+#     sheet = workbook.active
+#
+#     for row in sheet.iter_rows(min_row=2, values_only=True):
+#         title, year, director, genre = row
+#         movie = {
+#             'title': title,
+#             'year': int(year),
+#             'director': director,
+#             'genre': genre
+#         }
+#         movies.append(movie)
+#
+#     return movies
+#
+#
+# def movies_view(request):
+#     file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files/movies.xlsx')
+#     movies = read_movies_from_exel(file_path)
+#     return render(request, 'index.html', {'movies': movies})
 
 
+parcel_weight = int(input("Введите вес грамм"))
+print(letter(parcel_weight))
