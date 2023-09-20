ca0f9627965b07a5305b0f83888651a710b67ac2
Alena <yurevichei@mail.ru>
2023-09-20 21:02:31 +0300

    Вывод результата с точкой

--- calc/views.py
+++ calc/views.py
@@ -21,10 +21,14 @@ def read_letter_from_exel(filepath):
     return price_list
 
 
+
 def calculation_view(request):
-    file_path2 = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files/letter.xlsx')
-    price_list = read_letter_from_exel(file_path2)
-    return render(request, 'index.html', {'price_list': price_list})
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files/letter.xlsx')
+    price_list = read_letter_from_exel(file_path)
+    workbook = load_workbook(filename=file_path)
+    sheet = workbook.active
+    cost_of_delivery = "{:.2f}".format(sheet['B4'].value * 2)
+    return render(request, 'index.html', {'price_list': price_list, 'cost_of_delivery': cost_of_delivery})
 
 
 
