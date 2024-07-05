72e249dac4f785064110a9ec47df2077dafadf91
Alena <yurevichei@mail.ru>
2024-07-05 17:42:42 +0300

    about page images

--- calc/urls.py
+++ calc/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
 from django.views.generic import TemplateView
 from .views import calculation_view
-from .views import ems_view, page_not_found_view, about_view
-from django.conf.urls import handler404
+from .views import ems_view, about_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -12,7 +13,4 @@ urlpatterns = [
     path('about', about_view, name='about'),
     re_path(r'^sitemap.xml$', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml')),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
-]
-
-
-# handler404 = 'page_not_found_view'
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- postcalc2/settings.py
+++ postcalc2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tc6#cr4u)t)h*5p2^_yna$t*8g6sb)ei46!v=(0zx-dqi0tl7x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'calc/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
