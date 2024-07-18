2660badab3f803b9522e9694fcc337bf81931ea9
Alena <yurevichei@mail.ru>
2024-07-18 15:56:33 +0300

    testing

--- calc/notification.py
+++ calc/notification.py
@@ -1,10 +1,23 @@
-notification_list = [0.65, 2.10, 0.45]
+notification_costs = {
+    1: 0.65,
+    2: 2.10,
+    3: 0.45,
+    4: 0.00,
+}
 
 
 def notification_cost(notification):
-    print("обратились к функции notification_cost")
-    match notification:
-        case 1: return notification_list[0]
-        case 2: return notification_list[1]
-        case 3: return notification_list[2]
-        case 4: return 0
+    if not isinstance(notification, int) or notification not in notification_costs:
+        raise ValueError("Invalid notification type. Must be one of: 1, 2, 3, 4.")
+    return notification_costs.get(notification)
+
+# def notification_cost(notification):
+#     print("обратились к функции notification_cost")
+#     match notification:
+#         case 1: return notification_list[0]
+#         case 2: return notification_list[1]
+#         case 3: return notification_list[2]
+#         case 4: return 0
+
+
+print(notification_cost(4))
--- calc/tests.py
+++ calc/tests.py
@@ -18,29 +18,42 @@ def test_simple():
 
 
 def test_registered():
-    assert cost_of_registered(1000) == [{
+    assert cost_of_registered(1000, 0) == [{
             'fiz': '5,70',
             'yur': '5,70',
             'item_vat': '0,95',
             'for_declared': '',
             'tracking': 'да',
-            'rub': " руб."}]
-    assert cost_of_registered(30000) == [{
+            'rub': " руб.",
+            'notification': ""}]
+    assert cost_of_registered(30000, 0) == [{
         'fiz': 'Макс. вес 2 кг'
     }]
 
 
 def test_value_letter():
-    assert cost_of_value_letter(22, 10) == [{
-            'fiz': '3,66',
-            'yur': '4,02',
-            'item_vat': '0,67',
+    assert cost_of_value_letter(22, 10, 4) == [{
+            'fiz': '3,90',
+            'yur': '4,26',
+            'item_vat': '0,71',
             'for_declared': '0,36',
             'tracking': 'да',
             'sep': '/',
-            'rub': " руб."}]
-    assert cost_of_value_letter(2001, 10) == [{
+            'rub': " руб.",
+            'notification': ""
+    }]
+    assert cost_of_value_letter(2001, 10, 4) == [{
         'fiz': 'Макс. вес 2 кг', 'sep': ''
+    }]
+    assert cost_of_value_letter(22, 10, 1) == [{
+        'fiz': '4,68',
+        'yur': '5,04',
+        'item_vat': '0,84',
+        'for_declared': '0,36',
+        'tracking': 'да',
+        'sep': '/',
+        'rub': " руб.",
+        'notification': "0,78"
     }]
 
 
@@ -59,8 +72,8 @@ def test_first_class():
 
 def test_parcel():
     assert cost_of_parcel(900, '') == [{
-        'fiz': '3,00',
-        'yur': '6,22',
+        'fiz': '3,30',
+        'yur': '6,26',
         'item_vat': '1,04',
         'for_declared_fiz': '',
         'for_declared_yur': '',
@@ -70,9 +83,9 @@ def test_parcel():
         'sep2': '/',
     }]
     assert cost_of_parcel(510, '') == [{
-        'fiz': '3,00',
-        'yur': '5,94',
-        'item_vat': '0,99',
+        'fiz': '3,30',
+        'yur': '5,98',
+        'item_vat': '1,00',
         'for_declared_fiz': '',
         'for_declared_yur': '',
         'tracking': 'да',
@@ -81,9 +94,9 @@ def test_parcel():
         'sep2': '/',
     }]
     assert cost_of_parcel(915, 1.25) == [{
-        'fiz': '3,04',
-        'yur': '6,28',
-        'item_vat': '1,04',
+        'fiz': '3,34',
+        'yur': '6,34',
+        'item_vat': '1,06',
         'for_declared_fiz': '0,04',
         'for_declared_yur': '0,05',
         'tracking': 'да',
@@ -92,9 +105,9 @@ def test_parcel():
         'sep2': '/'
     }]
     assert cost_of_parcel(1150, '') == [{
-        'fiz': '4,90',
-        'yur': '6,48',
-        'item_vat': '1,08',
+        'fiz': '4,96',
+        'yur': '6,55',
+        'item_vat': '1,09',
         'for_declared_fiz': '',
         'for_declared_yur': '',
         'tracking': 'да',
@@ -104,9 +117,9 @@ def test_parcel():
 
     }]
     assert cost_of_parcel(1152, 10.12) == [{
-        'fiz': '5,17',
-        'yur': '6,80',
-        'item_vat': '1,13',
+        'fiz': '5,23',
+        'yur': '6,88',
+        'item_vat': '1,15',
         'for_declared_fiz': '0,30',
         'for_declared_yur': '0,36',
         'tracking': 'да',
