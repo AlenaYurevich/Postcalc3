d04202a9f95f062f3acf7fa5004eb5fbc7279807
Alena <yurevichei@mail.ru>
2024-07-24 16:17:15 +0300

    parcel with notification

--- calc/parcel.py
+++ calc/parcel.py
@@ -5,6 +5,7 @@ from .round_as_excel import round_as_excel
 from .format import formatted
 from .vat import vat
 from .declared_value import cost_for_declared_value
+from .notification import notification_cost
 
 
 """
@@ -18,6 +19,7 @@ from .declared_value import cost_for_declared_value
 
 
 file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files/letter2.xlsx')  # второй файл
+print("Открыли второй файл")
 workbook = load_workbook(filename=file_path)
 sheet = workbook.active
 
@@ -29,8 +31,9 @@ def weight(item_weight, declared_value):
         return math.ceil(item_weight / 10) / 100
 
 
-def cost_of_parcel(item_weight, declared_value):
+def cost_of_parcel(item_weight, declared_value, notification):
     price_row = []
+    notification = notification_cost(notification)
     if item_weight <= 50000:
         if item_weight <= 1000:
             if declared_value in ("нет", "", 0, "0"):
@@ -42,8 +45,6 @@ def cost_of_parcel(item_weight, declared_value):
                 yur = round_as_excel(yur)
                 for_declared_fiz = ''
                 for_declared_yur = ''
-                item_vat = vat(yur)
-                yur += item_vat
                 sep1 = ''
                 sep2 = '/'
             else:
@@ -53,8 +54,6 @@ def cost_of_parcel(item_weight, declared_value):
                 for_declared_fiz = cost_for_declared_value(declared_value)
                 yur += cost_for_declared_value(declared_value)
                 for_declared_yur = round_as_excel(cost_for_declared_value(declared_value)) * 1.2
-                item_vat = vat(yur)
-                yur += item_vat
                 sep1, sep2 = "/", "/"
         else:
             if declared_value in ("нет", "", 0, "0"):
@@ -63,8 +62,6 @@ def cost_of_parcel(item_weight, declared_value):
                 yur = round_as_excel(yur)
                 for_declared_fiz = ''
                 for_declared_yur = ''
-                item_vat = vat(yur)
-                yur += item_vat
                 sep1 = ''
                 sep2 = '/'
             else:
@@ -75,20 +72,25 @@ def cost_of_parcel(item_weight, declared_value):
                 for_declared_fiz = cost_for_declared_value(declared_value)
                 yur += cost_for_declared_value(declared_value)
                 for_declared_yur = round_as_excel(cost_for_declared_value(declared_value) * 1.2)
-                item_vat = vat(yur)
-                yur += item_vat
                 sep1, sep2 = "/", "/"
-
+        fiz += notification * 1.2
+        yur += notification
+        notification = notification * 1.2
+        if notification == 0:
+            notification = ""
+        item_vat_yur = round_as_excel(vat(yur))
+        yur = round_as_excel(yur + item_vat_yur)
         rate = {
             'fiz': fiz,
             'yur': yur,
-            'item_vat': item_vat,
+            'item_vat_yur': item_vat_yur,
             'for_declared_fiz': for_declared_fiz,
             'for_declared_yur': for_declared_yur,
             'rub': " руб.",
             'tracking': "да",
             'sep1': sep1,
             'sep2': sep2,
+            'notification': notification,
             }
         for i in rate:
             rate[i] = formatted(rate[i])
