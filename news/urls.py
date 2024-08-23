c3eef55b4031a81d4b0be3ed935875060977dde3
Alena <yurevichei@mail.ru>
2024-08-23 21:19:04 +0300

    adding posts

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path("cats/", views.categories)
+    path('', views.news_index, name='news_index'),
+    path('<int:pk>/', views.news_detail, name='news_detail'),
+    path('<category>/', views.news_category, name='news_category'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,9 +1,30 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
 
 
-def index(request):
-    return HttpResponse(request, "Страница приложения news")
+def news_index(request):
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, 'news_index.html', context)
 
 
-def categories(request):
-    return HttpResponse(request, "<h1>Статьи по категориям</h1>")
+def news_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        "post": post
+    }
+    return render(request, 'news_detail.html', context)
+
+
+def news_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on')
+    context = {
+        'category': category,
+        'posts': posts  # здесь выводим посты Posts
+    }
+    return render(request, 'news_category.html', context)
