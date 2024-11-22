75a9a860d264c8474600d57a4b459ef0047eb400
Alena <yurevichei@mail.ru>
2024-11-22 14:26:39 +0300

    ems int 02

--- calc/ems_int.py
+++ calc/ems_int.py
@@ -1,4 +1,3 @@
-import math
 from .sheets import sheet8
 from .vat import vat
 from .round_as_excel import round_as_excel
@@ -16,63 +15,51 @@ from .declared_value import cost_for_declared_value
 """
 
 
-def weight(item_weight, declared_value):
-    if declared_value in ("нет", "", 0, "0"):
-        return math.ceil(item_weight / 100) / 10
-    else:
-        return math.ceil(item_weight / 10) / 100  # повторяется код с parcel
-
-
-def find_numbers_by_country(row_number, priority):
-    if row_number < 11:
+def find_numbers_by_country(row_number, item):
+    if row_number < 10:
         # Если номер строки меньше 11, возвращаем None, потому что строки выше 11 не обрабатываются.
         return None
         # Получаем строку по её индексу.
     row_data = list(sheet8.iter_rows(min_row=row_number, max_row=row_number, values_only=True))
     if row_data:  # Проверка, что строка не пустая
         row_data = row_data[0]  # Извлекаем данные строки (поскольку iter_rows возвращает список кортежей)
-        if priority == "non_priority":
+        if item == "documents":
             data = (row_data[3], row_data[4])  # Получаем данные из 3-й и 4-й колонок
         else:
-            data = (row_data[5], row_data[6])  # Получаем данные из 5-й и 6-й колонок
+            data = (row_data[5], row_data[6], row_data[7])  # Получаем данные из 5 6 7 колонок
         return data
     # Если row_data пустой или None, возвращаем None, None
     return None
 
 
-def find_parcel_int_cost(destination, item_weight, declared_value, priority):
+def find_item_cost(destination, weight, declared_value, item):
     price_row = []
-    col = find_numbers_by_country(destination, priority)
+    col = find_numbers_by_country(destination, item)
     if type(col[0]) is str:
-        return [{'fiz': "Посылки не принимаются"}]
-    if declared_value in ("нет", "", 0, "0"):
-        fiz = col[0] + round_as_excel(col[1] * weight(item_weight, declared_value))
-        yur = fiz
-        for_declared_fiz = ''
-        for_declared_yur = ''
-        sep1 = ''
-        sep2 = '/'
+        return [{'fiz': "отправления не принимаются"}]
+    if weight <= 500:
+        fiz = col[0]
+    elif weight <= 1000:
+        fiz = col[1]
     else:
-        fiz = 5.85 + col[0] + col[1] * weight(item_weight, declared_value)
-        fiz = round(fiz, 4)
-        for_declared_fiz = cost_for_declared_value(declared_value)
-        fiz += for_declared_fiz
+        fiz = col[1] + col[2]
+    yur = fiz
+    if declared_value in ("нет", "", 0, "0"):
+        for_declared = ''
+    else:
+        for_declared = cost_for_declared_value(declared_value)
+        fiz += for_declared
         yur = fiz
         yur = round_as_excel(yur)
-        for_declared_yur = round_as_excel(cost_for_declared_value(declared_value)) * 1.2
-        sep1, sep2 = "/", "/"
+        for_declared = round_as_excel(cost_for_declared_value(declared_value)) * 1.2
     item_vat_yur = vat(yur)
-    yur = round_as_excel(yur + item_vat_yur)
+    fiz = round_as_excel(fiz + item_vat_yur)
+    yur = fiz
     rate = {
         'fiz': fiz,
         'yur': yur,
-        'item_vat_yur': item_vat_yur,
-        'for_declared_fiz': for_declared_fiz,
-        'for_declared_yur': for_declared_yur,
-        'rub': " руб.",
-        'tracking': "да",
-        'sep1': sep1,
-        'sep2': sep2,
+        'item_vat': item_vat_yur,
+        'for_declared': for_declared
     }
     for key in rate:
         rate[key] = formatted(rate[key])
@@ -80,11 +67,16 @@ def find_parcel_int_cost(destination, item_weight, declared_value, priority):
     return price_row
 
 
-def cost_of_parcel_int(destination, item_weight, declared_value):
-    if item_weight > 50000:
-        return [
-            [{'fiz': "Макс. вес 50 кг", 'yur': "-", 'item_vat_yur': "-", 'for_declared': "-"}],
-            [{'fiz': "Макс. вес 50 кг", 'yur': "-", 'item_vat_yur': "-", 'for_declared': "-"}],
-        ]
-    return [find_parcel_int_cost(destination, item_weight, declared_value, "non_priority"),
-            find_parcel_int_cost(destination, item_weight, declared_value, "priority")]
+def find_documents_cost(destination, weight, declared_value):
+    if weight > 1000:
+        return [{'fiz': "Макс. вес 1 кг", 'yur': "-", 'item_vat': "-"}]
+    return find_item_cost(destination, weight, declared_value, 'documents')
+
+
+def find_goods_cost(destination, weight, declared_value):
+    if weight > 30000:
+        return [{'fiz': "Макс. вес 30 кг", 'yur': "-", 'item_vat': "-"}]
+    return find_item_cost(destination, weight, declared_value, 'goods')
+
+
+print(find_goods_cost(12, 500, 0))
